ec1470f07178bc4f51f536a17137674514012495
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-12-05 22:13:18 +0900

    History settings to enjoy Huffshell

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -6,3 +6,8 @@ function save_command() {
 	fi
 }
 precmd_functions=($precmd_functions save_command)
+
+# overwrite oh-my-zsh to enjoy huffshell
+setopt no_hist_expire_dups_first
+setopt no_hist_ignore_dups
+setopt no_share_history
