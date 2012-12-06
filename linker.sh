be73701cf23dcc6e442913df857727d86ce8a6c7
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-12-07 06:20:29 +0900

    Make symlinker for my bundles

--- linker.sh
+++ linker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-function symlink {
+# ==================================================
+# Dotfiles
+# ==================================================
+function symlink_dot {
 	symlink_src=$1
 	symlink_dest=.$1
-	CONFIRM=y
+	CONFIRM=n
 	if [ -e $HOME/$symlink_dest ]; then
 		echo -n "$symlink_dest exists. override? [y/n]"
 		read CONFIRM
@@ -17,14 +20,27 @@ function symlink {
 	esac
 }
 
-symlink vimrc
-symlink vim
-symlink zshrc
-symlink tmux.conf
-symlink ctags
-symlink latexmkrc
+symlink_dot vimrc
+symlink_dot vim
+symlink_dot zshrc
+symlink_dot tmux.conf
+symlink_dot ctags
+symlink_dot latexmkrc
 
 if [ ! -L $ZSH/themes/genki.zsh-theme ]; then
 	echo "ln -s $HOME/dotfiles/genki.zsh-theme $ZSH/themes/genki.zsh-theme"
 	ln -s $HOME/dotfiles/genki.zsh-theme $ZSH/themes/genki.zsh-theme
 fi
+
+# ==================================================
+# My bundles
+# ==================================================
+function symlink_bundle {
+	src=$MYBUNDLES/$1
+	dest=$HOME/bin/${1##*/} # trim directory(ies)
+	dest=${dest%%.*} # trim extension(s)
+	echo "ln -s $src $dest"
+	ln -s $src $dest
+}
+
+symlink_bundle js-beautify/python/js-beautify
