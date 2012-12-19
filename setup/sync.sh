10d0c41eb38eb1f44a8080ad962186a3f9727a37
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-12-19 12:35:31 +0900

    Auto sync when actual file is specified. And sync firefox profiles.ini

--- setup/sync.sh
+++ setup/sync.sh
@@ -2,6 +2,25 @@
 
 sync_root=$HOME/Dropbox/sync_root
 
+function sync_it {
+	if [ $# -ne 1 ]; then
+		echo "Usage: $0 SYNC_FILE"
+		return
+	fi
+	local target=$1
+	if [ ! -e $target ]; then
+		echo "$target does not exist. Abort."
+		return
+	fi
+	if [ -L $target ]; then
+		echo "$target is symlink (maybe already synced?). Abort."
+		return
+	fi
+	# TODO: comfirm if it is ok to move
+	mkdir -p $sync_root${target%/*}
+	mv $target $sync_root$target
+}
+
 function sync_linker {
 	if [ $# -ne 1 ]; then
 		echo "Usage: $0 SYNC_FILE"
@@ -10,14 +29,23 @@ function sync_linker {
 	local link=$1
 	local file=$sync_root$1
 	if [ -e $link -a ! -L $link ]; then
-		echo "$link exists and not a symlink file. Abort."
-		return
+		echo "$link exists and not a symlink file."
+		echo "Moving $link to appropreate directory..."
+		sync_it $link
+	fi
+	if [ -L $link ]; then
+		# Remove existing symlink
+		rm $link
 	fi
 	if [ ! -e $file ]; then
-		echo "Warning: $file does not exist."
+		echo "$file does not exist. Abort."
+		return
 	fi
-	echo "ln -s" $file $link
 	ln -s $file $link
 }
 
+# Vimperator
 sync_linker $HOME/.vimperator
+
+# Firefox
+sync_linker $HOME/.mozilla/firefox/profiles.ini
