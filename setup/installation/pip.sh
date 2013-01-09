9406a78b9a3875584777442b1a567fc50e2261f3
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-01-09 21:47:25 +0900

    Add pip installation script

---
+++ setup/installation/pip.sh
@@ -0,0 +1,7 @@
+# Date: 2013-01-09
+# Install pip to manage python packages
+# Ref: http://www.saltycrane.com/blog/2010/02/how-install-pip-ubuntu/
+
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
