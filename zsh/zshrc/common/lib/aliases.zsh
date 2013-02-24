885d2bbbb15aebbe7faba856abe02b70a9fb2ce8
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-02-24 16:13:24 +0900

    Force git prompt reflesh after exiting vim

--- zsh/zshrc/common/lib/aliases.zsh
+++ zsh/zshrc/common/lib/aliases.zsh
@@ -1,12 +1,3 @@
-alias vim="echo 'You can issue v alias instead.'"
-alias v="vimentor vim --servername genki"
-alias ev="v ~/dotfiles/vimrc"
-alias ez="v ~/dotfiles/zshrc"
-alias et="v ~/dotfiles/tmux.conf"
-
-alias neobundle="v -u $HOME/dotfiles/bundle_vimrc $HOME/dotfiles/bundle_vimrc"
-alias nb="neobundle"
-
 alias hist="fc -liD 1"
 
 # ==================================================
---
+++ zsh/zshrc/common/lib/vim.zsh
@@ -0,0 +1,14 @@
+function myvim() {
+	vimentor vim --servername genki $*
+	__EXECUTED_GIT_COMMAND=1
+}
+
+alias vim="echo 'You can issue v alias instead.'"
+alias v="myvim"
+alias ev="v ~/dotfiles/vimrc"
+alias ez="v ~/dotfiles/zshrc"
+alias et="v ~/dotfiles/tmux.conf"
+
+alias neobundle="v -u $HOME/dotfiles/bundle_vimrc $HOME/dotfiles/bundle_vimrc"
+alias nb="neobundle"
+
