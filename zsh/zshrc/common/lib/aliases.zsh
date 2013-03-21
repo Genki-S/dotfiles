53700838e2fc8fb48495cbbceaf9aff2dd1a6301
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-03-22 06:29:40 +0900

    Add frequently used git command aliases

--- zsh/zshrc/common/lib/aliases.zsh
+++ zsh/zshrc/common/lib/aliases.zsh
@@ -22,3 +22,7 @@ alias sz="source $HOME/.zshrc"
 # Git
 # ==================================================
 alias gf="git diff --color"
+alias gr="git reset"
+alias grh~="git reset HEAD~"
+alias grh\^="git reset 'HEAD^'"
+alias gcam="git commit --amend"
