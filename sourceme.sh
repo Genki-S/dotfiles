033ceefb205687564d32704a1fafb25d97eda02b
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-03-22 05:01:01 +0900

    Source setups from dotfiles root directory

--- sourceme.sh
+++ sourceme.sh
@@ -4,3 +4,9 @@ git submodule update
 
 # If you have problem with antigen about https connection
 # sed -i 's/https/http/g' antigen.zsh
+
+# Source setups
+for f in $HOME/dotfiles/setup/*.sh
+do
+	source $f
+done
