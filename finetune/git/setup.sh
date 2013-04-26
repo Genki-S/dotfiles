5f7b89ff4dd21ede6a99098dacacc10301e071e9
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-04-26 10:52:45 +0900

    Add git user information setup

--- finetune/git/setup.sh
+++ finetune/git/setup.sh
@@ -1,2 +1,5 @@
+git config --global user.name "Genki"
+git config --global user.email cfhoyuk.reccos.nelg@gmail.com
+
 git config --global init.templatedir '~/.git_template'
 git config --global core.excludesfile '~/.gitignore_global'
