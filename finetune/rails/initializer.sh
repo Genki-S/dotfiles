9f66e588ebb77b466ec4c459bd51e5f58107b21e
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-05-01 06:24:29 +0900

    Add rails mongoid setting script (halfway)

--- finetune/rails/initializer.sh
+++ finetune/rails/initializer.sh
@@ -19,3 +19,9 @@ cp $MyRailsDir/env_genki.rb config/environments/genki.rb
 sed -i "" -e "s/<REPLACEME:ApplicationName>/$ApplicationName/g" config/environments/genki.rb
 # git filter
 echo "config/application.rb filter=rails_genki_env" >> .git/info/attributes
+
+# Mongoid settings
+# Basically, follow the instructions at http://mongoid.org/en/mongoid/docs/installation.html
+# This is also useful http://d.hatena.ne.jp/babie/20100809/1281316972
+rails generate mongoid:config
+rm config/database.yml
