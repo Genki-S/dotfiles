bb8d4547901d3674c6ca459b623312a2920c1ddb
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-05-01 16:54:31 +0900

    Move zsh files into shallower directory

--- zshfiles/rcfiles/common/lib/env.zsh
+++
@@ -1,5 +0,0 @@
-export MANPAGER="$SHELL -c \"col -b | vim -c 'setfiletype man' -\""
-export LESSCHARSET="utf-8"
-
-# Report time if a program takes more than 3 seconds
-REPORTTIME=3
---
+++ zshfiles/rcfiles/env.zsh
@@ -0,0 +1,5 @@
+MANPAGER="$SHELL -c \"col -b | vim -c 'setfiletype man' -\""
+LESSCHARSET="utf-8"
+
+# Report time if a program takes more than 3 seconds
+REPORTTIME=3
