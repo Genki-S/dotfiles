bc14c6438d7291e9c987202b9009014593d783d1
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-08 11:46:08 +0900

    Clone neobundle only if it does not exist

--- setup/vim.sh
+++ setup/vim.sh
@@ -3,7 +3,9 @@
 mkdir -p $HOME/.vimbundles
 
 # Install neobundle
-git clone http://github.com/Shougo/neobundle.vim $HOME/.vimbundles/neobundle.vim
+if [ ! -d $HOME/.vimbundles/neobundle.vim ]; then
+	git clone http://github.com/Shougo/neobundle.vim $HOME/.vimbundles/neobundle.vim
+fi
 
 # Neobundle will do the rest.
 command vim -c "NeoBundleInstall" -c "quit" || ;
