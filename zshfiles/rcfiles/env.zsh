37c0757d6df576c6131c0128b2617289cae5afeb
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-10 18:54:38 +0900

    Say hello to VIM env variable which is absolute path to vim executable

--- zshfiles/rcfiles/env.zsh
+++ zshfiles/rcfiles/env.zsh
@@ -1,5 +1,10 @@
-MANPAGER="$SHELL -c \"col -b | vim -c 'setfiletype man' -\""
-LESSCHARSET="utf-8"
+if [[ $PLATFORM == "mac" ]]; then
+	VIM=/usr/local/bin/vim
+elif [[ $PLATFORM == "linux" ]]; then
+fi
+
+export MANPAGER="$SHELL -c \"col -b | $VIM -c 'setfiletype man' -\""
+export LESSCHARSET="utf-8"
 
 # Report time if a program takes more than 3 seconds
 REPORTTIME=3
