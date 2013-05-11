33d3738e7508d50f1d67d3979bbd1212990a5169
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-11 12:49:09 +0900

    Add geeknote aliases

---
+++ zshfiles/rcfiles/geeknote.zsh
@@ -0,0 +1,24 @@
+alias geeknote="python $MYBUNDLES/geeknote/geeknote.py"
+alias gn="geeknote"
+alias gnc="gn create"
+alias gne="gn edit"
+alias gnf="gn find"
+alias gns="gn show"
+alias gnrm="gn remove"
+alias gnbls="gn notebook-list"
+alias gnbc="gn notebook-create"
+alias gnbe="gn notebook-edit"
+alias gntls="gn tag-list"
+alias gntc="gn tag-create"
+alias gnte="gn tag-edit"
+
+alias gnsync="python $MYBUNDLES/geeknote/gnsync.py"
+
+# Journaling function
+function jl() {
+	echo -n "Title: "
+	read title
+	echo -n "Tags(space separated): "
+	read tags
+	geeknote create --title $title --content "WRITE" --tags $tags --notebook Journal
+}
