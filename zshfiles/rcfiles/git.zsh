39af5353f580431509d88b92e7ed260c86486176
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-15 11:15:35 +0900

    Use hub as git

--- zshfiles/rcfiles/git.zsh
+++ zshfiles/rcfiles/git.zsh
@@ -1,6 +1,9 @@
+# Use hub
+alias git=hub
+
+# Sugers
 alias gf="git diff --color"
 alias gr="git reset"
 alias grh~="git reset HEAD~"
 alias grh\^="git reset 'HEAD^'"
 alias gcam="git commit --amend"
-
