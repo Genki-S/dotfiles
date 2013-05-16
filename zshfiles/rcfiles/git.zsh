7c9a355724067ae40f92161380c8469a18fc5d84
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-17 06:05:29 +0900

    Add settings for ghi auth

--- zshfiles/rcfiles/git.zsh
+++ zshfiles/rcfiles/git.zsh
@@ -4,6 +4,9 @@ function git() {
 	__EXECUTED_GIT_COMMAND=1
 }
 
+# ghi -- github issues from command line
+export GHI_TOKEN=`cat ~/private_files/ghi_token`
+
 # Sugers
 alias gf="git diff --color"
 alias gr="git reset"
