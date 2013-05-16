3d3f1fe4b0e4bea9ad7a48322970bddad2217add
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-17 06:26:33 +0900

    Add git aliases
    Inspired by
    http://codeinthehole.com/writing/pull-requests-and-other-good-practices-for-teams-using-github/

--- zshfiles/rcfiles/git.zsh
+++ zshfiles/rcfiles/git.zsh
@@ -9,7 +9,16 @@ export GHI_TOKEN=`cat ~/private_files/ghi_token`
 
 # Sugers
 alias gf="git diff --color"
+alias gfc="git diff --color --cached"
 alias gr="git reset"
 alias grh~="git reset HEAD~"
 alias grh\^="git reset 'HEAD^'"
 alias gcam="git commit --amend"
+alias gt="git tag"
+alias grb="git rebase"
+alias grbc="git rebase --continue"
+alias grbs="git rebase --skip"
+alias gsl="git stash list"
+alias gss="git stash save"
+alias gsf="git stash show"
+alias gsp="git stash pop"
