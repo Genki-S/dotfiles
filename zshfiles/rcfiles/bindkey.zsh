ec6fc5a17e4e88ec9a3866849b4bcc5ebecafaa2
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-21 08:18:20 +0900

    Implement zvi-set-dirmark and zvi-jump-dirmark

--- zshfiles/rcfiles/bindkey.zsh
+++ zshfiles/rcfiles/bindkey.zsh
@@ -15,6 +15,33 @@ bindkey -M vicmd 'dd' kill-whole-line
 bindkey -M vicmd 'u' undo
 bindkey -M vicmd '^r' redo
 
+# Directory Mark
+typeset -A zvi_dirmarks
+# Load marks from database
+zvi_dirmarks_save_file=$HOME/.zvi_dirmarks
+source $zvi_dirmarks_save_file
+
+autoload -U read-from-minibuffer
+
+function zvi-set-dirmark {
+	local REPLY
+	read-from-minibuffer -k 1 "Mark:"
+	zvi_dirmarks[$REPLY]=`pwd`
+	# Save marks
+	echo "zvi_dirmarks=(${(kv)zvi_dirmarks})" > $zvi_dirmarks_save_file
+}
+function zvi-jump-dirmark {
+	local REPLY
+	read-from-minibuffer -k 1 "Mark:"
+	if [[ -n $zvi_dirmarks[$REPLY] ]]; then
+		cd $zvi_dirmarks[$REPLY]
+	fi
+	zle reset-prompt
+}
+zle -N zvi-set-dirmark
+zle -N zvi-jump-dirmark
+bindkey -M vicmd 'm' zvi-set-dirmark
+bindkey -M vicmd '`' zvi-jump-dirmark
 
 # ==================================================
 # Common settings
