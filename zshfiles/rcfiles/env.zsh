d169c6e7462cfcbcfe8947d2b1419a3e93527dc9
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-20 16:17:49 +0900

    Revert "Say hello to VIM env variable which is absolute path to vim executable"
    This reverts commit 37c0757d6df576c6131c0128b2617289cae5afeb.

--- zshfiles/rcfiles/env.zsh
+++ zshfiles/rcfiles/env.zsh
@@ -1,10 +1,5 @@
-if [[ $PLATFORM == "mac" ]]; then
-	VIM=/usr/local/bin/vim
-elif [[ $PLATFORM == "linux" ]]; then
-fi
-
-export MANPAGER="$SHELL -c \"col -b | $VIM -c 'setfiletype man' -\""
-export LESSCHARSET="utf-8"
+MANPAGER="$SHELL -c \"col -b | vim -c 'setfiletype man' -\""
+LESSCHARSET="utf-8"
 
 # Report time if a program takes more than 3 seconds
 REPORTTIME=3
