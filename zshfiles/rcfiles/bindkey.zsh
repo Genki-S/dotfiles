255a878c8c329c6cf70979cdf5ffcbf449efd999
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-21 06:54:59 +0900

    Use vi keybind

--- zshfiles/rcfiles/bindkey.zsh
+++ zshfiles/rcfiles/bindkey.zsh
@@ -1,6 +1,45 @@
+# Use vi keybind
+bindkey -v
+
+# ==================================================
+# Viins settings
+# ==================================================
+
+# ==================================================
+# Vicmd settings
+# ==================================================
+bindkey -M vicmd 'yy' vi-yank-whole-line
+bindkey -M vicmd 'Y' vi-yank-eol
+bindkey -M vicmd 'cc' vi-change-whole-line
+bindkey -M vicmd 'dd' kill-whole-line
+bindkey -M vicmd 'u' undo
+bindkey -M vicmd '^r' redo
+
+
+# ==================================================
+# Common settings
+# ==================================================
+bindkey '^p' history-substring-search-up
+bindkey '^n' history-substring-search-down
+
+# Use <C-@> to edit command line with $EDITOR
 autoload edit-command-line
 zle -N edit-command-line
 bindkey '^@' edit-command-line
+
+# ==================================================
+# For prompt
+# ==================================================
+function zle-line-init zle-keymap-select {
+	zle reset-prompt
+}
+zle -N zle-keymap-select
+
+function vi_command_mode_prompt_info() {
+	if [[ $KEYMAP = "vicmd" ]]; then
+		echo "M: VICMD"
+	fi
+}
 
 # ==================================================
 # My widgets
