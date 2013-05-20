7091d9e8e4fe6b9da639ed085f1cbaf513b2db8c
Genki <cfhoyuk.reccos.nelg@gmail.com>
2013-05-20 16:19:05 +0900

    Export MANPAGER and LESSCHARSET

--- zshfiles/rcfiles/env.zsh
+++ zshfiles/rcfiles/env.zsh
@@ -1,5 +1,5 @@
-MANPAGER="$SHELL -c \"col -b | vim -c 'setfiletype man' -\""
-LESSCHARSET="utf-8"
+export MANPAGER="$SHELL -c \"col -b | vim -c 'setfiletype man' -\""
+export LESSCHARSET="utf-8"
 
 # Report time if a program takes more than 3 seconds
 REPORTTIME=3
