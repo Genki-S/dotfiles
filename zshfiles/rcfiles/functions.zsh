a3ff62d1d4d29108cd80a3672015bbc129305436
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-06-13 05:26:08 +0900

    Write very useful function cdup: change directory upwards

--- zshfiles/rcfiles/functions.zsh
+++ zshfiles/rcfiles/functions.zsh
@@ -8,3 +8,25 @@ function noproxy () {
 	https_proxy=
 	ftp_proxy=
 }
+
+# Usage: cdup pattern
+# search pattern for upper directories and cd to first matched directory
+# e.g. cwd: /super/very/deep/directory -> `cdup ery` -> cwd: /super/very
+function cdup () {
+	if [ $# -ne 1 ]; then
+		echo "Usage: $0 pattern"
+		return 1
+	fi
+
+	local pat=$1
+	local cwd=$PWD
+	while [ $cwd != '/' ] ; do
+		local dirname=${cwd##*/}
+		if [[ $dirname =~ $pat ]]; then
+			cd $cwd
+			return 0
+		fi
+		cwd=${cwd%/*}
+	done
+	return 1
+}
