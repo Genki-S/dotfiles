c32800fdf648cea09ec891b9ed63744fdb682e03
Genki Sugimoto <genki-sugimoto@cookpad.com>
2013-08-02 10:26:24 +0900

    Very useful notification after long command

---
+++ zshfiles/rcfiles/notification.zsh
@@ -0,0 +1,26 @@
+# Got ideas from these:
+#   http://superuser.com/questions/553564/is-there-a-way-to-make-zsh-run-a-command-after-reporttime
+#   http://brettterpstra.com/2012/08/02/a-drop-in-growlnotify-replacement-for-mountain-lion-notifications/
+#   https://github.com/alloy/terminal-notifier
+
+if [[ -x `which terminal-notifier` ]]; then
+	notify-preexec-hook() {
+		zsh_notifier_cmd="$1"
+		zsh_notifier_time="`date +%s`"
+	}
+
+	notify-precmd-hook() {
+		local time_taken
+		if [[ "${zsh_notifier_cmd}" != "" ]]; then
+			time_taken=$(( `date +%s` - ${zsh_notifier_time}  ))
+			if (( $time_taken > $REPORTTIME  )); then
+				terminal-notifier -title "Task Finished" \
+					-message "'$zsh_notifier_cmd' exited after $time_taken seconds"
+			fi
+		fi
+		zsh_notifier_cmd=
+	}
+fi
+
+preexec_functions+=(notify-preexec-hook)
+precmd_functions+=(notify-precmd-hook)
