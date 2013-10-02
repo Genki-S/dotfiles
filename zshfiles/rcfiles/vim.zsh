d05d7a5c35ef6c5c00e3fd756f104cd58f58885a
Genki Sugimoto <genki-sugimoto@cookpad.com>
2013-10-02 15:22:16 +0900

    Remove asynccommand to not depend on +clientserver

--- zshfiles/rcfiles/vim.zsh
+++ zshfiles/rcfiles/vim.zsh
@@ -1,6 +1,6 @@
 function myvim() {
 	local startuptime_file="$HOME/.tmp/profiling/vim/`date "+%Y%m%d_%H%M%S"`"
-	vim --servername genki --startuptime $startuptime_file $*
+	vim --startuptime $startuptime_file $*
 	__EXECUTED_GIT_COMMAND=1
 	echo "vim startup: `tail -n1 $startuptime_file | cut -d' ' -f1` ms."
 }
