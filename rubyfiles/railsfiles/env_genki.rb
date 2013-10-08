985d173e06a3208603d7042187d479f10365f188
Genki Sugimoto <genki-sugimoto@cookpad.com>
2013-10-08 11:19:14 +0900

    Remove obsolete files

--- rubyfiles/railsfiles/env_genki.rb
+++
@@ -1,6 +0,0 @@
-Rails.env = "development"
-eval File.read("#{Rails.root}/config/environments/development.rb")
-
-<REPLACEME:ApplicationName>::Application.configure do
-  config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
-end
