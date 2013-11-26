958035c9e221b8346e0707ea460901e4631b891c
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-11-26 18:11:33 +0900

    Get rid of mybundles

--- zshfiles/envfiles/env.zsh
+++ zshfiles/envfiles/env.zsh
@@ -1,7 +1,6 @@
 # Directory sugars
 export DOTDIR=$HOME/dotfiles
 export WIDGETDIR=$DOTDIR/zshfiles/rcfiles/widgets
-export MYBUNDLES=$HOME/.mybundles
 
 # Preferences
 export TERM=xterm-256color
@@ -11,5 +10,4 @@ export EDITOR=vim
 export LANG=en_US.UTF-8
 
 # Miscellaneous
-export RSENSE_HOME=$MYBUNDLES/rsense
 export HTTP_HOME=http://vim.wikia.com/wiki/Special:Random
