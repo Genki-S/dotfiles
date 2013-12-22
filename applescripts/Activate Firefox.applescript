760b58176252411a2b977d48e3ffd216559f3831
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-12-22 12:14:45 +0900

    Add some mac configs and applescripts

---
+++ applescripts/Activate Firefox.applescript
@@ -0,0 +1,1 @@
+tell application "Firefox" to activate
---
+++ config/mac.sh
@@ -0,0 +1,5 @@
+# Disable dashboard
+defaults write com.apple.dashboard mcx-disabled -boolean YES
+
+# Global shortcuts
+# Use QuickSilver while I can be able to bind shortcuts to applescripts through commands
