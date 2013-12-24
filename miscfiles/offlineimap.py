0f611331abb48d08f7728a7176e9d566010cf2cf
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-12-25 05:56:14 +0900

    Fix offlineimap settings

--- miscfiles/offlineimap.py
+++ miscfiles/offlineimap.py
@@ -2,7 +2,7 @@
 import re, os
 
 def get_authinfo_password(machine, login, port):
-    s = "machine %s login %s port %s password ([^ ]*) port %s\n" % (machine, login, port)
+    s = "machine %s login %s password ([^ ]*) port %s\n" % (machine, login, port)
     p = re.compile(s)
     authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
     return p.search(authinfo).group(1)
