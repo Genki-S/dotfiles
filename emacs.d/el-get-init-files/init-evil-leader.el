2b06751d25e7c8205857446f4c692ee0f50891c5
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-03-12 16:28:42 +0900

    Add useful org mappings

--- emacs.d/el-get-init-files/init-evil-leader.el
+++ emacs.d/el-get-init-files/init-evil-leader.el
@@ -2,5 +2,8 @@
 (evil-leader/set-leader ",")
 
 (evil-leader/set-key-for-mode 'org-mode
+  "j" 'org-next-item
+  "k" 'org-previous-item
+  "h" 'org-up-element
   ">" 'org-demote-subtree
   "<" 'org-promote-subtree)
