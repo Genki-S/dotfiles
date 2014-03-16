c82fdd5fbedd7f556fd519f86cb1ffcb2f88fa48
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-03-16 12:19:44 +0900

    Revert "Show org-pomodoro state and duration in tmux status bar"
    This reverts commit 65b018984ad6ceb2c348c9d98d153bcd84d809df.
    
    Conflicts:
    	emacs.d/setup/setup-org.el

--- emacs.d/setup/setup-org.el
+++ emacs.d/setup/setup-org.el
@@ -4,44 +4,11 @@
 ; (require 'org-helpers)
 ; (require 'org-export)
 
-;; POMODORO SETTINGS
-;; http://headhole.org/organisation/2012/08/22/org-mode-gtd-and-the-pomodoro-technique/
-(require 'org-pomodoro)
-
-;; for tmux status bar
-(defun write-org-pomodoro-state (state)
-  (with-temp-buffer
-    (insert (concat state "\n"
-                    (format-time-string "%s") "\n"))
-    (when (file-writable-p "~/.org-pomodoro-status")
-      (write-region (point-min) (point-max) "~/.org-pomodoro-status")
-      (call-process "tmux-all-session-do" nil 0 nil "refresh-client" "-S"))))
-
 ;; notification https://gist.github.com/jstewart/7664823
 (defun notify-osx (title message)
   (call-process "osx-alert"
                 nil 0 nil
                 title message))
-
-(add-hook 'org-pomodoro-started-hook
-          (lambda ()
-            (write-org-pomodoro-state (symbol-name org-pomodoro-state))))
-(add-hook 'org-pomodoro-finished-hook
-          (lambda ()
-            (write-org-pomodoro-state (symbol-name org-pomodoro-state))
-            (notify-osx "Pomodoro completed!" "Time for a break.")))
-(add-hook 'org-pomodoro-short-break-finished-hook
-          (lambda ()
-            (write-org-pomodoro-state ":none")
-            (notify-osx "Pomodoro Short Break Finished" "Ready for Another?")))
-(add-hook 'org-pomodoro-long-break-finished-hook
-          (lambda ()
-            (write-org-pomodoro-state ":none")
-            (notify-osx "Pomodoro Long Break Finished" "Ready for Another?")))
-(add-hook 'org-pomodoro-killed-hook
-          (lambda ()
-            (write-org-pomodoro-state ":none")
-            (notify-osx "Pomodoro Killed" "One does not simply kill a pomodoro!")))
 
 ;; sets the default workflow keywords and their faces
 (setq org-todo-keywords
