fb1a3c2cc15b9f1c873dba5de226544ec5f73ec3
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-03-30 13:51:23 +0900

    Add clicoder.d

---
+++ clicoder.d/template.cpp
@@ -0,0 +1,139 @@
+// Header {{{
+// includes {{{
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <sys/time.h>
+#include <unistd.h>
+#include <vector>
+// }}}
+using namespace std;
+// consts {{{
+static const int INF = 1e9;
+static const double PI = acos(-1.0);
+static const double EPS = 1e-10;
+// }}}
+// typedefs {{{
+typedef long long int LL;
+typedef unsigned long long int ULL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VLL;
+typedef vector<VLL> VVLL;
+typedef vector<double> VD;
+typedef vector<VD> VVD;
+typedef vector<bool> VB;
+typedef vector<VB> VVB;
+typedef vector<char> VC;
+typedef vector<VC> VVC;
+typedef vector<string> VS;
+typedef vector<VS> VVS;
+typedef pair<int, int> PII;
+typedef complex<int> P;
+// }}}
+// macros & inline functions {{{
+// syntax sugars {{{
+#define FOR(i, b, e) for (typeof(e) i = (b); i < (e); ++i)
+#define FORI(i, b, e) for (typeof(e) i = (b); i <= (e); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REPI(i, n) FORI(i, 0, n)
+#define OPOVER(_op, _type) inline bool operator _op (const _type &t) const
+// }}}
+// conversion {{{
+inline int toInt(string s) { int v; istringstream sin(s); sin>>v; return v; }
+template<class T> inline string toString(T x) { ostringstream sout; sout<<x; return sout.str(); }
+// }}}
+// array and STL {{{
+#define ARRSIZE(a) ( sizeof(a) / sizeof(a[0]) )
+#define ZERO(a, v) ( assert(v == 0 || v == -1), memset(a, v, sizeof(a)) )
+#define F first
+#define S second
+#define MP(a, b) make_pair(a, b)
+#define SIZE(a) ((LL)a.size())
+#define PB(e) push_back(e)
+#define SORT(v) sort((v).begin(), (v).end())
+#define RSORT(v) sort((v).rbegin(), (v).rend())
+#define ALL(a) (a).begin(), (a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define EACH(c, it) for(typeof((c).begin()) it=(c).begin(); it!=(c).end(); ++it)
+#define EXIST(s, e) ((s).find(e) != (s).end())
+// }}}
+// bit manipulation {{{
+#define BIT(n) (assert(n < 64), (1ULL << (n)))
+#define BITOF(n, m) (assert(m < 64), ((ULL)(n) >> (m) & 1))
+inline int onbits_count(ULL b) { int c = 0; while(b != 0) { c += (b & 1); b >>= 1; } return c; }
+inline int bits_count(ULL b) { int c = 0; while(b != 0) { ++c; b >>= 1; } return c; }
+// }}}
+// debug {{{
+#define dprt(fmt, ...) if (opt_debug) fprintf(stderr, fmt, ##__VA_ARGS__)
+#define darr(a) if (opt_debug) copy( (a), (a) + arrsz(a), ostream_iterator<int>(cerr, " ") ); cerr << endl
+#define darr_range(a, f, t) if (opt_debug) copy( (a) + (f), (a) + (t), ostream_iterator<int>(cerr, " ") ); cerr << endl
+#define dvec(v) if (opt_debug) copy( ALL(v), ostream_iterator<int>(cerr, " ") ); cerr << endl
+#define darr2(a, n, m) if (opt_debug) FOR(i, 0, (n)){ darr_range( (a)[i], 0, (m) ); }
+#define dvec2(v) if (opt_debug) FOR(i, 0, SZ(v)){ dvec( (v)[i] ); }
+#define WAIT() if (opt_debug) { string _wait_; cerr << "(hit return to continue)" << endl; getline(cin, _wait_); }
+#define dump(x) if (opt_debug) cerr << " [L" << __LINE__ << "] " << #x << " = " << (x) << endl;
+#define dumpf() if (opt_debug) cerr << __PRETTY_FUNCTION__ << endl;
+#define dumpv(x) if (opt_debug) cerr << " [L:" << __LINE__ << "] " << #x << " = "; REP(q, (x).size()) cerr << (x)[q] << " "; cerr << endl;
+#define where() if (opt_debug) cerr << __FILE__ << ": " << __PRETTY_FUNCTION__ << " [L: " << __LINE__ << "]" << endl;
+#define show_bits(b, s) if(opt_debug) { REP(i, s) { cerr << BITOF(b, s-1-i); if(i%4 == 3) cerr << ' '; } cerr << endl; }
+template<typename T1, typename T2> ostream& operator<<(ostream& s, const pair<T1, T2>& d) {return s << "(" << d.first << ", " << d.second << ")";}
+// }}}
+// }}}
+// time {{{
+inline double now(){ struct timeval tv; gettimeofday(&tv, NULL); return (static_cast<double>(tv.tv_sec) + static_cast<double>(tv.tv_usec) * 1e-6); }
+// }}}
+// string manipulation {{{
+inline VS split(string s, char delimiter) { VS v; string t; REP(i, s.length()) { if(s[i] == delimiter) v.PB(t), t = ""; else t += s[i]; } v.PB(t); return v; }
+// }}}
+// geometry {{{
+#define Y real()
+#define X imag()
+// }}}
+// 2 dimentional array {{{
+P dydx4[4] = { P(-1, 0), P(0, 1), P(1, 0), P(0, -1) };
+P dydx8[8] = { P(-1, 0), P(0, 1), P(1, 0), P(0, -1), P(-1, 1), P(1, 1), P(1, -1), P(-1, -1) };
+int g_height, g_width;
+bool in_field(P p) {
+	return (0 <= p.Y && p.Y < g_height) && (0 <= p.X && p.X < g_width);
+}
+// }}}
+// }}}
+bool opt_debug = false;
+
+int main(int argc, char** argv) {
+	std::ios_base::sync_with_stdio(false);
+	// set options {{{
+	int __c;
+	while ( (__c = getopt(argc, argv, "d")) != -1 ) {
+		switch (__c) {
+			case 'd':
+				opt_debug = true;
+				break;
+			default:
+				abort();
+		}
+	}
+	// }}}
+
+	<`0`>
+}
+
+// vim: foldmethod=marker
