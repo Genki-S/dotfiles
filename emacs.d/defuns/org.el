f4b453e321f31940e51ec0d9bcfe769827e9196d
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-04-19 08:49:10 +0900

    Fix so that org task to tmux function can handle capture buffer

--- emacs.d/defuns/org.el
+++ emacs.d/defuns/org.el
@@ -1,10 +1,11 @@
+(defun genki/tmux-set-status-right (message)
+  (call-process "tmux"
+                nil 0 nil
+                "set" "status-right" message))
+
 (defun genki/org-clock-current-task-to-tmux-statusline ()
   "Show current task on tmux statusline"
-  (call-process "tmux"
-                nil 0 nil
-                "set" "status-right" org-clock-current-task))
+  (genki/tmux-set-status-right (or org-clock-current-task "CAPTURE")))
 
 (defun genki/org-clock-out-hook ()
-  (call-process "tmux"
-                nil 0 nil
-                "set" "status-right" "NOT CLOCKING IN..."))
+  (genki/tmux-set-status-right "NOT CLOCKING IN..."))
