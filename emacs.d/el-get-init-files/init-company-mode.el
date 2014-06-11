15589f2762f73ba728d8ae1cd7b7b1bcc8e4e917
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-06-11 15:47:55 +0900

    Add dirty hack to change company keymaps

--- emacs.d/el-get-init-files/init-company-mode.el
+++ emacs.d/el-get-init-files/init-company-mode.el
@@ -2,3 +2,36 @@
 (add-hook 'company-mode-hook
           (lambda ()
             (add-to-list 'company-backends '(company-yasnippet company-ispell))))
+
+;; dirty hack to change company-active-map
+(defvar company-active-map
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap "\e\e\e" 'company-abort)
+    (define-key keymap "\C-g" 'company-abort)
+    (define-key keymap (kbd "C-n") 'company-select-next) ; change
+    (define-key keymap (kbd "C-p") 'company-select-previous) ; change
+    (define-key keymap (kbd "<down>") 'company-select-next-or-abort)
+    (define-key keymap (kbd "<up>") 'company-select-previous-or-abort)
+    (define-key keymap [down-mouse-1] 'ignore)
+    (define-key keymap [down-mouse-3] 'ignore)
+    (define-key keymap [mouse-1] 'company-complete-mouse)
+    (define-key keymap [mouse-3] 'company-select-mouse)
+    (define-key keymap [up-mouse-1] 'ignore)
+    (define-key keymap [up-mouse-3] 'ignore)
+    (define-key keymap [return] 'company-complete-selection)
+    (define-key keymap (kbd "RET") 'company-complete-selection)
+    (define-key keymap [tab] 'company-complete-common)
+    (define-key keymap (kbd "TAB") 'company-complete-common)
+    (define-key keymap (kbd "<f1>") 'company-show-doc-buffer)
+    ; (define-key keymap (kbd "C-h") 'company-show-doc-buffer) ; change
+    ; (define-key keymap "\C-w" 'company-show-location) ; change
+    (define-key keymap "\C-s" 'company-search-candidates)
+    (define-key keymap "\C-\M-s" 'company-filter-candidates)
+    (dotimes (i 10)
+      (define-key keymap (vector (+ (aref (kbd "M-0") 0) i))
+        `(lambda ()
+           (interactive)
+           (company-complete-number ,(if (zerop i) 10 i)))))
+
+    keymap)
+  "Keymap that is enabled during an active completion.")
