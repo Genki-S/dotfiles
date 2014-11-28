4fb665412f88ddc4cc19d3793dd4894a4fcadd11
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-11-28 18:20:53 +0900

    Make history search unique

--- zshfiles/rcfiles/peco.zsh
+++ zshfiles/rcfiles/peco.zsh
@@ -1,7 +1,15 @@
 function peco_select_history() {
 	local tac
 	executable gtac && tac="gtac" || { executable tac && tac="tac" || { tac="tail -r" } }
-	BUFFER=$(builtin history -n 1 | eval $tac | peco --query "$LBUFFER")
+	BUFFER=$( \
+		builtin history 1 \
+		| sort -k2 \
+		| uniq --skip-fields=1 \
+		| sort -n -k1 \
+		| sed 's/^ *//' \
+		| cut -d' ' -f 2- \
+		| sed 's/^ *//' \
+		| peco --query "$LBUFFER")
 	CURSOR=$#BUFFER # move cursor
 	zle -R -c # refresh
 }
