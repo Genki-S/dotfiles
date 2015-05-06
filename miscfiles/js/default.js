5f6dd4dbe4a64a7a6429e23383da322eb490b9a4
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-05-07 07:05:06 +0900

    Disable custom hinttags

--- miscfiles/js/default.js
+++ miscfiles/js/default.js
@@ -1,4 +1,4 @@
-var clickables = $('*').filter(function() {
-  return $(this).css('cursor') == 'pointer';
-});
-clickables.addClass('vimperator-hint-target');
+// var clickables = $('*').filter(function() {
+  // return $(this).css('cursor') == 'pointer';
+// });
+// clickables.addClass('vimperator-hint-target');
