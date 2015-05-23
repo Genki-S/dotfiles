04d8b9abaab02a23897f6fd1b8924b311a3bd340
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-05-23 12:09:23 +0900

    Make C-h in helm delete backward char, C-w delete backward word

--- emacs.d/el-get-init-files/init-helm.el
+++ emacs.d/el-get-init-files/init-helm.el
@@ -1,1 +1,5 @@
 (helm-mode 1)
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-map (kbd "C-w") 'evil-delete-backward-word)
+(define-key helm-find-files-map (kbd "C-w") 'evil-delete-backward-word)
