409116840bfd62c9d9c4c7c9c0cb656087d06f11
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-04 16:41:48 +0900

    Fix zsh notification

--- zshfiles/rcfiles/notification.zsh
+++ zshfiles/rcfiles/notification.zsh
@@ -3,22 +3,37 @@
 #   http://brettterpstra.com/2012/08/02/a-drop-in-growlnotify-replacement-for-mountain-lion-notifications/
 #   https://github.com/alloy/terminal-notifier
 
+__ZSH_NOTIFIER_EXCLUDE_COMMANDS=(
+	myvim
+	git
+	tig
+)
+
 if executable terminal-notifier; then
 	notify-preexec-hook() {
 		zsh_notifier_cmd="$1"
+		zsh_notifier_raw_cmd="$2"
 		zsh_notifier_time="`date +%s`"
 	}
 
 	notify-precmd-hook() {
+		local raw_cmd=${zsh_notifier_raw_cmd%% *}
+		if [ ${__ZSH_NOTIFIER_EXCLUDE_COMMANDS[(i)${raw_cmd}]} -le ${#__ZSH_NOTIFIER_EXCLUDE_COMMANDS} ]; then
+			return
+		fi
+
 		local time_taken
 		if [[ "${zsh_notifier_cmd}" != "" ]]; then
 			time_taken=$(( `date +%s` - ${zsh_notifier_time}  ))
 			if (( $time_taken > $REPORTTIME  )); then
-				terminal-notifier -title "Task Finished" \
+				# FIXME: dirty hack to use reattach-to-user-namespece
+				#        see: https://github.com/alloy/terminal-notifier/issues/115
+				reattach-to-user-namespace terminal-notifier -title "Task Finished" \
 					-message "'$zsh_notifier_cmd' exited after $time_taken seconds"
 			fi
 		fi
 		zsh_notifier_cmd=
+		zsh_notifier_raw_cmd=
 	}
 fi
 
