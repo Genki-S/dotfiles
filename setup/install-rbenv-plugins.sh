e13d9050e70936412d9706d11b86378592415fee
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-06 07:51:37 +0900

    Use rbenv-default-gems

--- setup/install-rbenv-plugins.sh
+++ setup/install-rbenv-plugins.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/amatsuda/gem-src.git $RBENV_ROOT/plugins/gem-src
+set -x
+
+git clone https://github.com/amatsuda/gem-src.git $RBENV_ROOT/plugins/gem-src || true
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git $RBENV_ROOT/plugins/rbenv-gem-rehash || true
+git clone https://github.com/sstephenson/rbenv-default-gems.git $RBENV_ROOT/plugins/rbenv-default-gems || true
+# FIXME: hack to deal with boxen-installed rbenv (resides in /opt/boxen/rbenv)
+cp $HOME/dotfiles/rubyfiles/rbenv/default-gems $RBENV_ROOT/default-gems
