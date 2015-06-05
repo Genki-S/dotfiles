e4c67d9d81ef200ad972d9c30e8e30a5f1659511
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-05 16:01:03 +0900

    Update helm and modify helm settings

--- emacs.d/el-get-init-files/init-helm.el
+++ emacs.d/el-get-init-files/init-helm.el
@@ -1,4 +1,10 @@
+(require 'helm)
+(require 'helm-files)
+
 (helm-mode 1)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-map (kbd "C-w") 'evil-delete-backward-word)
