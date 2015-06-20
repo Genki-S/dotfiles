b2dcc8807c3a46f420337efebd1f1bfabca49f40
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-21 08:55:13 +0900

    Add unionfind to the library

---
+++ clicoder.d/library/unionfind.cpp
@@ -0,0 +1,29 @@
+class UnionFindTree {
+	int n;
+	VI parent;
+	public:
+		UnionFindTree(int _n): n(_n) {
+			parent = VI(n);
+			REP (i, n) {
+				parent[i] = i;
+			}
+		}
+
+		int group(int x) {
+			if (parent[x] == x) {
+				return x;
+			} else {
+				return parent[x] = group(parent[x]);
+			}
+		}
+
+		bool united(int x, int y) {
+			return group(x) == group(y);
+		}
+
+		void unite(int x, int y) {
+			x = group(x);
+			y = group(y);
+			parent[x] = y;
+		}
+};
