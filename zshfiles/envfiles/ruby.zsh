f32d87f77b85e90b4362fe536490603d1a4bc699
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-06-23 20:08:47 +0900

    Stop rbenv from rehashing all the time

--- zshfiles/envfiles/ruby.zsh
+++ zshfiles/envfiles/ruby.zsh
@@ -1,5 +1,5 @@
 # Source rbenv environment
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv > /dev/null; then eval "$(rbenv init - --no-rehash)"; fi
 
 # Tell bundler to use local Gemfile
 function set_bundle_gemfile () {
