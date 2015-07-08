a3f56b4a88ac9bd12aebf38ac810d60b7c18cd78
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-07-08 12:28:24 +0900

    Set PYTHONIOENCODING to utf-8

--- zshfiles/envfiles/env.zsh
+++ zshfiles/envfiles/env.zsh
@@ -13,5 +13,11 @@ export LANG=en_US.UTF-8
 # GOLANG
 export GOPATH=$HOME/go
 
+# Python
+# Deal with python scripts which does not encode outputs
+# See: http://stackoverflow.com/questions/492483/setting-the-correct-encoding-when-piping-stdout-in-python
+#      http://stackoverflow.com/questions/22442490/weird-python-encoding-error-in-vim-with-make
+export PYTHONIOENCODING=utf-8
+
 # Miscellaneous
 export HTTP_HOME=http://vim.wikia.com/wiki/Special:Random
