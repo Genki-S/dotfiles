d88d8675ff02f1cbcc16b499bcc1d2c590d2d787
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-07-12 23:15:50 +0900

    Install more gems as rbenv plugins

--- setup/install-rbenv-plugins.sh
+++ setup/install-rbenv-plugins.sh
@@ -6,4 +6,7 @@ git clone https://github.com/amatsuda/gem-src.git $RBENV_ROOT/plugins/gem-src ||
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git $RBENV_ROOT/plugins/rbenv-gem-rehash || true
 git clone https://github.com/sstephenson/rbenv-default-gems.git $RBENV_ROOT/plugins/rbenv-default-gems || true
 git clone https://github.com/tpope/gem-ctags $RBENV_ROOT/plugins/gem-ctags || true
+git clone https://github.com/tpope/gem-ctags $RBENV_ROOT/plugins/gem-ctags || true
+git clone https://github.com/julienXX/terminal-notifier /tmp/terminal-notifier || true
+rm -rf $RBENV_ROOT/plugins/terminal-notifier && cp -R /tmp/terminal-notifier/Ruby $RBENV_ROOT/plugins/terminal-notifier && rm -rf /tmp/terminal-notifier
 cp $HOME/dotfiles/rubyfiles/rbenv/default-gems $RBENV_ROOT/default-gems
