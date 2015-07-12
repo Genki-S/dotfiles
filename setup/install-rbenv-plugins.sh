482dcfe6495a7188bd6fe4fb37e4324373b907ff
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-07-12 10:36:56 +0900

    Install tpope/gem-ctags as rbenv plugin

--- setup/install-rbenv-plugins.sh
+++ setup/install-rbenv-plugins.sh
@@ -5,4 +5,5 @@ set -x
 git clone https://github.com/amatsuda/gem-src.git $RBENV_ROOT/plugins/gem-src || true
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git $RBENV_ROOT/plugins/rbenv-gem-rehash || true
 git clone https://github.com/sstephenson/rbenv-default-gems.git $RBENV_ROOT/plugins/rbenv-default-gems || true
+git clone https://github.com/tpope/gem-ctags $RBENV_ROOT/plugins/gem-ctags || true
 cp $HOME/dotfiles/rubyfiles/rbenv/default-gems $RBENV_ROOT/default-gems
