c88f2e7697cc01a752d593018c16698aa6ce07ff
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2016-06-02 15:06:24 +0900

    Modify zsh long run command notification to work on Ubuntu

--- zshfiles/rcfiles/notification.zsh
+++ zshfiles/rcfiles/notification.zsh
@@ -3,6 +3,11 @@
 #   http://brettterpstra.com/2012/08/02/a-drop-in-growlnotify-replacement-for-mountain-lion-notifications/
 #   https://github.com/alloy/terminal-notifier
 
+ring()
+{
+	echo -n ''
+}
+
 __ZSH_NOTIFIER_EXCLUDE_COMMANDS=(
 	myvim
 	git
@@ -10,37 +15,35 @@ __ZSH_NOTIFIER_EXCLUDE_COMMANDS=(
 	fg
 )
 
-if executable terminal-notifier; then
-	notify-preexec-hook() {
-		zsh_notifier_cmd="$1"
-		zsh_notifier_raw_cmd="$2"
-		zsh_notifier_time="`date +%s`"
-	}
+notify-preexec-hook() {
+	zsh_notifier_cmd="$1"
+	zsh_notifier_raw_cmd="$2"
+	zsh_notifier_time="`date +%s`"
+}
 
-	notify-precmd-hook() {
-		local raw_cmd=${zsh_notifier_raw_cmd%% *}
-		if [ ${__ZSH_NOTIFIER_EXCLUDE_COMMANDS[(i)${raw_cmd}]} -le ${#__ZSH_NOTIFIER_EXCLUDE_COMMANDS} ]; then
-			return
-		fi
+notify-precmd-hook() {
+	local raw_cmd=${zsh_notifier_raw_cmd%% *}
+	if [ ${__ZSH_NOTIFIER_EXCLUDE_COMMANDS[(i)${raw_cmd}]} -le ${#__ZSH_NOTIFIER_EXCLUDE_COMMANDS} ]; then
+		return
+	fi
 
-		local time_taken
-		if [[ "${zsh_notifier_cmd}" != "" ]]; then
-			time_taken=$(( `date +%s` - ${zsh_notifier_time}  ))
-			if (( $time_taken > $REPORTTIME  )); then
+	local time_taken
+	time_taken=$(( `date +%s` - ${zsh_notifier_time}  ))
+
+	if [[ "${zsh_notifier_cmd}" != "" ]]; then
+		if (( $time_taken > $REPORTTIME  )); then
+			if is_mac && executable terminal-notifier; then
 				# FIXME: dirty hack to use reattach-to-user-namespece
 				#        see: https://github.com/alloy/terminal-notifier/issues/115
 				reattach-to-user-namespace terminal-notifier -title "Task Finished" \
 					-message "'$zsh_notifier_raw_cmd' exited after $time_taken seconds"
+			elif executable espeak; then
+				echo "${zsh_notifier_raw_cmd%% *} has finished" | espeak
 			fi
 		fi
-		zsh_notifier_cmd=
-		zsh_notifier_raw_cmd=
-	}
-fi
-
-ring()
-{
-	echo -n ''
+	fi
+	zsh_notifier_cmd=
+	zsh_notifier_raw_cmd=
 }
 
 preexec_functions+=(notify-preexec-hook)
