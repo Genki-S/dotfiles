691b7c4fc39740dea6289e4b39ae3ddf528e3f4b
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2016-06-03 15:14:10 +0900

    Install rbenv-ctags using default-gems

--- setup/install-rbenv-plugins.sh
+++ setup/install-rbenv-plugins.sh
@@ -5,5 +5,4 @@ set -x
 git clone https://github.com/amatsuda/gem-src.git $RBENV_ROOT/plugins/gem-src || true
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git $RBENV_ROOT/plugins/rbenv-gem-rehash || true
 git clone https://github.com/sstephenson/rbenv-default-gems.git $RBENV_ROOT/plugins/rbenv-default-gems || true
-git clone https://github.com/tpope/rbenv-ctags.git $RBENV_ROOT/plugins/rbenv-ctags || true
 cp $HOME/dotfiles/rubyfiles/rbenv/default-gems $RBENV_ROOT/default-gems
