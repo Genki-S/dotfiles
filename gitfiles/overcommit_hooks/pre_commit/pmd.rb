b5b6e9c5ff40bce5eeb8bf8974119470f432a26d
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2016-07-06 13:30:48 +0900

    Configure PMD rules

--- gitfiles/overcommit_hooks/pre_commit/pmd.rb
+++ gitfiles/overcommit_hooks/pre_commit/pmd.rb
@@ -1,12 +1,31 @@
 module Overcommit::Hook::PreCommit
   class Pmd < Base
+    WARNING_MESSAGE_REGEXPS = [
+      /A class which only has private constructors should be final/,
+      /Avoid using Literals in Conditional Statements/,
+      /Too many static imports may lead to messy code/,
+      /This class has too many methods, consider refactoring it/,
+      /The String literal [^ ]* appears \d+ times in this file/,
+    ]
+
     def run
       rulesets = config['rulesets'].map { |s| s.gsub(/\$[A-Z]+/) { |m| ENV[m[1..-1]] } }
       result = execute(command, args: ['-d', applicable_files.join(','), '-f', config['format'], '-R', rulesets.join(',')])
-      return :pass if result.success?
-
       output = result.stdout + result.stderr
-      [:fail, output]
+
+      error_messages = output.split("\n").delete_if do |line|
+        WARNING_MESSAGE_REGEXPS.any? { |pat| pat.match(line) }
+      end
+
+      unless error_messages.empty?
+        return [:fail, error_messages.join("\n")]
+      end
+
+      if output.empty?
+        return [:pass]
+      else
+        return [:warn, output]
+      end
     end
   end
 end
