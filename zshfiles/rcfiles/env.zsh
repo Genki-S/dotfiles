b964a418c169762c5417ec2c047acf6e603ac76d
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2016-10-28 17:04:42 +0900

    Kill the lag when entering vi-cmd mode on zle

--- zshfiles/rcfiles/env.zsh
+++ zshfiles/rcfiles/env.zsh
@@ -2,3 +2,6 @@ export LESSCHARSET="utf-8"
 
 # Report time if a program takes more than 3 seconds
 REPORTTIME=3
+
+# Kill the lag when entering vi-cmd: https://dougblack.io/words/zsh-vi-mode.html
+KEYTIMEOUT=1
