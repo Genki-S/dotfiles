dfad5aba1d2ba6f43e01e30a34e40d3c2ee060b6
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2018-12-05 11:44:44 +0900

    zsh: Setup fasd and magic-space widget for best cd'ing experience
    Also: remove unused cdd from submodules (j<Space> is the best
    cd'ing experience so far)

--- zshfiles.old/rcfiles/cdd.zsh
+++
@@ -1,3 +0,0 @@
-source ~/dotfiles/submodules/cdd/cdd
-
-chpwd_functions+=(_cdd_chpwd)
--- zshfiles/rcfiles/zle.zsh
+++ zshfiles/rcfiles/zle.zsh
@@ -1,3 +1,21 @@
 # Use emacs bindings in insert mode, but provide some vi-like bindings
 bindkey -e
 bindkey '^[' vi-cmd-mode
+
+
+() {
+  local widget_dir=$HOME/dotfiles/zshfiles/rcfiles/widgets
+
+  fpath=(
+    $fpath
+    $widget_dir
+  )
+
+  for wgt in $widget_dir/*
+  do
+    autoload -U $wgt:t
+    zle -N $wgt:t
+  done
+}
+
+bindkey ' ' magic-space
