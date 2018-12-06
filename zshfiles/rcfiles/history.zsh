5f137a9d798c17c5ccd9a06230fdbba64289c574
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2018-12-06 15:17:53 +0900

    zsh: Setup history (mostly copied from oh-my-zsh)

---
+++ zshfiles/rcfiles/history.zsh
@@ -0,0 +1,13 @@
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+# man zshoptions
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt no_inc_append_history
+setopt share_history
