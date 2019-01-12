4f3a4abe26660331fd14835936f1497bdc000bbb
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2019-01-12 21:30:54 +0900

    zsh: Make it possible to play sound with long-running command notification
    NOTIFY_SUCCESS_SOUNDS and NOTIFY_FAILURE_SOUNDS are meant to be set in
    `~/.zshrc.local`.

--- zshfiles/rcfiles/hooks.zsh
+++ zshfiles/rcfiles/hooks.zsh
@@ -13,6 +13,9 @@ NOTIFY_EXCLUDE_COMMANDS=(
   man
 )
 
+NOTIFY_SUCCESS_SOUNDS=()
+NOTIFY_FAILURE_SOUNDS=()
+
 notify-preexec-hook() {
   zsh_notifier_cmd="$1"
   zsh_notifier_raw_cmd="$2"
@@ -20,6 +23,8 @@ notify-preexec-hook() {
 }
 
 notify-precmd-hook() {
+  local exit_code=$?
+
   if [ -z "$zsh_notifier_time" ]; then
     return
   fi
@@ -34,7 +39,21 @@ notify-precmd-hook() {
 
   if [[ "${zsh_notifier_cmd}" != "" ]]; then
     if (( $time_taken > $REPORTTIME_SECONDS  )); then
-      my-notification "Command finished" "$raw_cmd exited after $time_taken seconds"
+      my-notification "Command finished" "$raw_cmd exited after $time_taken seconds with exit code $exit_code"
+      if [[ ${#NOTIFY_SUCCESS_SOUNDS} -gt 0 && $exit_code -eq 0 ]]; then
+        local len=${#NOTIFY_SUCCESS_SOUNDS}
+        local idx=$(( $[${RANDOM}%${len}] + 1 ))
+        local sound=${NOTIFY_SUCCESS_SOUNDS[${idx}]}
+        # double fork (so that this won't be counted as background job of current shell)
+        # https://stackoverflow.com/a/20338327
+        ( ffplay -nodisp -autoexit "$sound" &> /dev/null & )
+      fi
+      if [[ ${#NOTIFY_FAILURE_SOUNDS} -gt 0 && $exit_code -ne 0 ]]; then
+        local len=${#NOTIFY_FAILURE_SOUNDS}
+        local idx=$(( $[${RANDOM}%${len}] + 1 ))
+        local sound=${NOTIFY_FAILURE_SOUNDS[${idx}]}
+        ( ffplay -nodisp -autoexit "$sound" &> /dev/null & )
+      fi
     fi
   fi
   zsh_notifier_cmd=
