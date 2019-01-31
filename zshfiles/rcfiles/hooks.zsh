b1abcc0a6e4e3416a7397cd30d1975348a5b89bd
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2019-01-31 15:19:26 +0900

    zsh: Define "NLCF" as "Notify Long-running Command Finish" feature

--- zshfiles/rcfiles/hooks.zsh
+++ zshfiles/rcfiles/hooks.zsh
@@ -1,11 +1,11 @@
-# Notify when long-running command finishes
+# NLCF: Notify Long-running Command Finish {{{
 # Got ideas from these:
 #   http://superuser.com/questions/553564/is-there-a-way-to-make-zsh-run-a-command-after-reporttime
 #   http://brettterpstra.com/2012/08/02/a-drop-in-growlnotify-replacement-for-mountain-lion-notifications/
 #   https://github.com/alloy/terminal-notifier
 
 REPORTTIME_SECONDS=3
-NOTIFY_EXCLUDE_COMMANDS=(
+NLCF_EXCLUDE_COMMANDS=(
   nvim
   git
   tig
@@ -13,16 +13,16 @@ NOTIFY_EXCLUDE_COMMANDS=(
   man
 )
 
-NOTIFY_SUCCESS_SOUNDS=()
-NOTIFY_FAILURE_SOUNDS=()
+NLCF_SUCCESS_SOUNDS=()
+NLCF_FAILURE_SOUNDS=()
 
-notify-preexec-hook() {
+nlcf-preexec-hook() {
   zsh_notifier_cmd="$1"
   zsh_notifier_raw_cmd="$2"
   zsh_notifier_time="`date +%s`"
 }
 
-notify-precmd-hook() {
+nlcf-precmd-hook() {
   local exit_code=$?
 
   if [ -z "$zsh_notifier_time" ]; then
@@ -30,7 +30,7 @@ notify-precmd-hook() {
   fi
 
   local raw_cmd=${zsh_notifier_raw_cmd%% *}
-  if [ ${NOTIFY_EXCLUDE_COMMANDS[(i)${raw_cmd}]} -le ${#NOTIFY_EXCLUDE_COMMANDS} ]; then
+  if [ ${NLCF_EXCLUDE_COMMANDS[(i)${raw_cmd}]} -le ${#NLCF_EXCLUDE_COMMANDS} ]; then
     return
   fi
 
@@ -40,18 +40,18 @@ notify-precmd-hook() {
   if [[ "${zsh_notifier_cmd}" != "" ]]; then
     if (( $time_taken > $REPORTTIME_SECONDS  )); then
       my-notification "Command finished" "$raw_cmd exited after $time_taken seconds with exit code $exit_code"
-      if [[ ${#NOTIFY_SUCCESS_SOUNDS} -gt 0 && $exit_code -eq 0 ]]; then
-        local len=${#NOTIFY_SUCCESS_SOUNDS}
+      if [[ ${#NLCF_SUCCESS_SOUNDS} -gt 0 && $exit_code -eq 0 ]]; then
+        local len=${#NLCF_SUCCESS_SOUNDS}
         local idx=$(( $[${RANDOM}%${len}] + 1 ))
-        local sound=${NOTIFY_SUCCESS_SOUNDS[${idx}]}
+        local sound=${NLCF_SUCCESS_SOUNDS[${idx}]}
         # double fork (so that this won't be counted as background job of current shell)
         # https://stackoverflow.com/a/20338327
         ( ffplay -nodisp -autoexit -volume 20 "$sound" &> /dev/null & )
       fi
-      if [[ ${#NOTIFY_FAILURE_SOUNDS} -gt 0 && $exit_code -ne 0 ]]; then
-        local len=${#NOTIFY_FAILURE_SOUNDS}
+      if [[ ${#NLCF_FAILURE_SOUNDS} -gt 0 && $exit_code -ne 0 ]]; then
+        local len=${#NLCF_FAILURE_SOUNDS}
         local idx=$(( $[${RANDOM}%${len}] + 1 ))
-        local sound=${NOTIFY_FAILURE_SOUNDS[${idx}]}
+        local sound=${NLCF_FAILURE_SOUNDS[${idx}]}
         ( ffplay -nodisp -autoexit -volume 20 "$sound" &> /dev/null & )
       fi
     fi
@@ -59,6 +59,9 @@ notify-precmd-hook() {
   zsh_notifier_cmd=
   zsh_notifier_raw_cmd=
 }
+# }}}
 
-preexec_functions+=(notify-preexec-hook)
-precmd_functions+=(notify-precmd-hook ring)
+preexec_functions+=(nlcf-preexec-hook)
+precmd_functions+=(nlcf-precmd-hook ring)
+
+# vim: foldmethod=marker
