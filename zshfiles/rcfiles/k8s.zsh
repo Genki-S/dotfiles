9de8a3c876db6ff5b933e5c25190f641ac5aee3a
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2019-08-30 15:18:45 +0900

    zsh: Add some useful alias for kubectl

---
+++ zshfiles/rcfiles/k8s.zsh
@@ -0,0 +1,16 @@
+# aliases {{{
+alias k='kubectl'
+
+alias kg='k get'
+alias kgp='k get pods'
+alias kgn='k get nodes'
+alias kgs='k get secrets'
+
+alias kd='k describe'
+alias kdp='k describe pod'
+alias kdn='k describe node'
+
+alias kl='k logs'
+alias kr='k run'
+alias kc='k config'
+# }}}
