ac8866ae8e06198e98e6def76cceb3f94ccc649c
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2019-10-11 11:48:52 +0900

    zsh: Reintroduce the complete-from-tmux-pane

--- zshfiles/rcfiles/completion.zsh
+++ zshfiles/rcfiles/completion.zsh
@@ -1,2 +1,48 @@
 # case-insensitive completion
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
+
+# Complete words from tmux pane(s)
+# Source: http://blog.plenz.com/2012-01/zsh-complete-words-from-tmux-pane.html
+# Gist: https://gist.github.com/blueyed/6856354
+_tmux_pane_words() {
+  local expl
+  local -a w
+  if [[ -z "$TMUX_PANE" ]]; then
+    _message "not running inside tmux!"
+    return 1
+  fi
+
+  # Based on vim-tmuxcomplete's splitwords function.
+  # https://github.com/wellle/tmux-complete.vim/blob/master/sh/tmuxcomplete
+  _tmux_capture_pane() {
+    tmux capture-pane -J -p -S -100 $@ |
+      # Remove "^C".
+      sed 's/\^C\S*/ /g' |
+      # copy lines and split words
+      sed -e 'p;s/[^a-zA-Z0-9_]/ /g' |
+      # split on spaces
+      tr -s '[:space:]' '\n' |
+      # remove surrounding non-word characters
+      =grep -o "\w.*\w"
+  }
+  # Capture current pane first.
+  w=( ${(u)=$(_tmux_capture_pane)} )
+  echo $w > /tmp/w1
+  local i
+  for i in $(tmux list-panes -F '#D'); do
+    # Skip current pane (handled before).
+    [[ "$TMUX_PANE" = "$i" ]] && continue
+    w+=( ${(u)=$(_tmux_capture_pane -t $i)} )
+  done
+  _wanted values expl 'words from current tmux pane' compadd -a w
+}
+
+zle -C tmux-pane-words-prefix   complete-word _generic
+zle -C tmux-pane-words-anywhere complete-word _generic
+bindkey '^x^t' tmux-pane-words-prefix
+bindkey '^xT' tmux-pane-words-anywhere
+zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' completer _tmux_pane_words
+zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' ignore-line current
+# display the (interactive) menu on first execution of the hotkey
+zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' menu yes select interactive
+zstyle ':completion:tmux-pane-words-anywhere:*' matcher-list 'b:=* m:{A-Za-z}={a-zA-Z}'
