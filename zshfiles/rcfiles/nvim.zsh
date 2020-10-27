fea42b80a324c6eaaf1a5344100a71b309004918
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2020-10-27 10:57:14 +0900

    nvim: Play sound when exiting nvim
    To play "いいコード書いてる？" voice shared at
    https://makinomonaka.fanbox.cc/posts/768804
    
    :)

--- zshfiles/rcfiles/nvim.zsh
+++ zshfiles/rcfiles/nvim.zsh
@@ -1,5 +1,12 @@
-# aliases {{{
-alias v='nvim'
-# }}}
+function myvim() {
+  nvim $*
+  if [[ -n "$VIM_AFTER_SOUND" && -f "$VIM_AFTER_SOUND" ]]; then
+    # double fork (so that this won't be counted as background job of current shell)
+    # https://stackoverflow.com/a/20338327
+    ( ffplay -nodisp -autoexit -volume 100 "$VIM_AFTER_SOUND" &> /dev/null & )
+  fi
+}
+
+alias v='myvim'
 
 # vim:foldmethod=marker
